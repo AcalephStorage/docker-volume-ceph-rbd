fa0a17e03f08d8ba6eaf86a7bafdbc9d7a5dd36f
Alistair A. Israel <aisrael@gmail.com>
2015-06-29 18:49:47 +0800

    Rainbows and unicorns

--- driver.go
+++ driver.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"sync"
 
 	"github.com/calavera/dkvolume"
@@ -33,31 +35,91 @@ func newCephDriver(root, pool string) cephDriver {
 	return d
 }
 
-func (d cephDriver) Create(r dkvolume.Request) dkvolume.Response {
-	log.Printf("Create(%s)\n", r.Name)
-	d.m.Lock()
-	defer d.m.Unlock()
-	m := d.mountpoint(r.Name)
+func checkFs(device string) (string, error) {
+	log.Printf("blkid %s", device)
+ 	out, err := exec.Command("blkid", device).Output()
+ 	if out != nil {
+	 	sout := strings.TrimRight(string(out), "\n")
+		return sout, err
+ 	}
+ 	return "", err
+}
 
-	if _, ok := d.volumes[m]; ok {
-		return dkvolume.Response{}
+func ensureFs(name string) dkvolume.Response {
+	log.Printf("ensureFs(%q)", name)
+	device, err := rbdMap(name)
+	if err != nil {
+		return dkvolume.Response{Err: err.Error()}
 	}
+	defer rbdUnmap(*device)
 
-	// TODO Actually create an RBD
-
+	t, err := checkFs(*device)
+	log.Printf("t => %q", t)
+	if !strings.HasSuffix(t, "TYPE=\"xfs\"") {
+		log.Printf("Formatting %s", *device)
+		out, _, err := sh(fmt.Sprintf("mkfs.xfs -f %s", *device))
+		if err != nil {
+			return dkvolume.Response{Err: err.Error()}
+		}
+		log.Printf(string(out))
+	}
 	return dkvolume.Response{}
 }
 
-func (d cephDriver) Remove(r dkvolume.Request) dkvolume.Response {
-	log.Printf("Removing volume %s\n", r.Name)
+func rbdExists(name string) (bool, error) {
+	out, _, err := execCommand("rbd", "ls")
+	if err != nil {
+		return false, err
+	}
+	for _, s := range strings.Split(string(out), "\n") {
+		if name == s {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+func (d cephDriver) Create(r dkvolume.Request) dkvolume.Response {
+	log.Printf("Create(%q)", r.Name)
 	d.m.Lock()
 	defer d.m.Unlock()
-	m := d.mountpoint(r.Name)
+	name := r.Name
+	mountpoint := d.mountpoint(name)
 
-	if s, ok := d.volumes[m]; ok {
-		if s.connections <= 1 {
-			// TODO Actually delete the RBD
-			delete(d.volumes, m)
+	if _, ok := d.volumes[mountpoint]; ok {
+		return dkvolume.Response{}
+	}
+
+	exists, err := rbdExists(name)
+	if err != nil {
+		return dkvolume.Response{Err: err.Error()}
+	}
+
+	if !exists {
+		if out, _, err := sh(fmt.Sprintf("rbd create --size 128 %s", name)); err != nil {
+			log.Print(string(out))
+			return dkvolume.Response{Err: err.Error()}
+		}
+	}
+
+	return ensureFs(name)
+}
+
+func (d cephDriver) Remove(r dkvolume.Request) dkvolume.Response {
+	log.Printf("Remove(%q)", r.Name)
+	d.m.Lock()
+	defer d.m.Unlock()
+	name := r.Name
+	mountpoint := d.mountpoint(name)
+
+	if volume, ok := d.volumes[mountpoint]; ok {
+		if volume.connections <= 1 {
+			cmd := fmt.Sprintf("rbd rm %s", name)
+			if out, _, err := sh(cmd); err != nil {
+				log.Print(string(out))
+				return dkvolume.Response{Err: err.Error()}
+			}
+			delete(d.volumes, mountpoint)
 		}
 	}
 	return dkvolume.Response{}
@@ -68,15 +130,15 @@ func (d cephDriver) Path(r dkvolume.Request) dkvolume.Response {
 }
 
 func (d cephDriver) Mount(r dkvolume.Request) dkvolume.Response {
-	log.Printf("Mount(%s)\n", r.Name)
+	log.Printf("Mount(%q)", r.Name)
 	d.m.Lock()
 	defer d.m.Unlock()
 	mountpoint := d.mountpoint(r.Name)
-	log.Printf("mountpoint(%s) => %s\n", r.Name, mountpoint)
+	log.Printf("mountpoint(%q) => %q", r.Name, mountpoint)
 
-	s, ok := d.volumes[mountpoint]
-	if ok && s.connections > 0 {
-		s.connections++
+	vol, ok := d.volumes[mountpoint]
+	if ok && vol.connections > 0 {
+		vol.connections++
 		return dkvolume.Response{Mountpoint: mountpoint}
 	}
 
@@ -105,11 +167,11 @@ func (d cephDriver) Mount(r dkvolume.Request) dkvolume.Response {
 }
 
 func (d cephDriver) Unmount(r dkvolume.Request) dkvolume.Response {
-	log.Printf("Unmount(%s)\n", r.Name)
+	log.Printf("Unmount(%q)\n", r.Name)
 	d.m.Lock()
 	defer d.m.Unlock()
 	mountpoint := d.mountpoint(r.Name)
-	log.Printf("mountpoint(%s) => %s\n", r.Name, mountpoint)
+	log.Printf("mountpoint(%q) => %s\n", r.Name, mountpoint)
 
 	if volume, ok := d.volumes[mountpoint]; ok {
 		if volume.connections == 1 {
@@ -129,38 +191,77 @@ func (d *cephDriver) mountpoint(name string) string {
 	return filepath.Join(d.root, name)
 }
 
-func sh(cmd string) ([]byte, error) {
-	log.Println(cmd)
-	return exec.Command("sh", "-c", cmd).CombinedOutput()
+func execCommand(name string, arg... string) ([]byte, []byte, error) {
+ 	cmd := exec.Command(name, arg...)
+	stdout, err := cmd.StdoutPipe(); if err != nil {
+		return nil, nil, err
+	}
+ 	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return nil, nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, nil, err
+	}
+	sout, err := ioutil.ReadAll(stdout); if err != nil {
+		return nil, nil, err
+	}
+	serr, err := ioutil.ReadAll(stderr); if err != nil {
+		return nil, nil, err
+	}
+	if err := cmd.Wait(); err != nil {
+		return nil, nil, err
+	}
+	return sout, serr, nil
 }
 
-func (d *cephDriver) mountVolume(name, destination string) (*string, error) {
-	cmd := fmt.Sprintf("rbd map %s", name)
-	out, err := sh(cmd)
+func sh(s string) ([]byte, []byte, error) {
+	log.Print(s)
+	return execCommand("sh", "-c", s)
+}
+
+func rbdMap(name string) (*string, error) {
+	log.Printf("rbdMap(%q)", name)
+	out, _, err := sh(fmt.Sprintf("rbd map %s", name))
 	if err != nil {
-		log.Println(string(out))
+		log.Print(string(out))
 		return nil, err
 	}
-	log.Println(string(out))
-	device := string(out)
-	cmd = fmt.Sprintf("mount /dev/rbd1 %s", destination)
-	if out, err := sh(cmd); err != nil {
-		log.Println(string(out))
-		return nil, err
-	}
+	device := strings.TrimRight(string(out), "\n")
+	log.Printf("rbd map %s => %q", name, device)
 	return &device, nil
 }
 
-func (d *cephDriver) unmountVolume(target, device string) error {
-	cmd := fmt.Sprintf("umount %s", target)
-	if out, err := sh(cmd); err != nil {
-		log.Println(string(out))
-		return err
-	}
-	cmd = fmt.Sprintf("rbd unmap %s", device)
-	if out, err := sh(cmd); err != nil {
-		log.Println(string(out))
+func rbdUnmap(device string) error {
+	log.Printf("rbdUnmap(%q)", device)
+	if out, _, err := sh(fmt.Sprintf("rbd unmap %s", device)); err != nil {
+		log.Print(string(out))
 		return err
 	}
 	return nil
+}
+
+func (d *cephDriver) mountVolume(name, target string) (*string, error) {
+	log.Printf("mountVolume(%q, %q)", name, target)
+	device, err := rbdMap(name)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	log.Printf("rbdMap(%q) => %q", name, *device)
+	if out, _, err := sh(fmt.Sprintf("mount %s %s", *device, target)); err != nil {
+		log.Print(string(out))
+		return nil, err
+	}
+	return device, nil
+}
+
+func (d *cephDriver) unmountVolume(target, device string) error {
+	log.Printf("unmountVolume(%q, %q)", target, device)
+	cmd := fmt.Sprintf("umount %s", target)
+	if out, _, err := sh(cmd); err != nil {
+		log.Print(string(out))
+		return err
+	}
+	return rbdUnmap(device)
 }
